e41e9e46697caf86764f98b959d84a9a52d65fb6
pdychinco <prdychinco@gmail.com>
2025-03-11 21:13:38 -0700

    changed express.json line

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
